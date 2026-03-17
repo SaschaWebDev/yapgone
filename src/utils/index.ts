a7c9617ebd1752434fb3a6c5f1958c64fff79fe1
Sascha Majewsky <saschawebdev@pm.me>
2026-03-17 10:57:12 +0100

    feat(constants): add gallery and poll configuration limits

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@
 export { linkify } from './linkify'
 export { formatMessage } from './format-message'
 export { computeWaveform } from './compute-waveform'
+export { isEmojiOnly } from './is-emoji-only'
